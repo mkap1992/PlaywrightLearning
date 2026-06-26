7778bef094b10da8bd6d400e9821a519d745857b
npatil <nagarjuna.patil@infor.com>
2026-06-26 07:12:33 +0530

    Fix: Add type annotations to add function parameters

---
+++ Opps_TypeScript/Chapter_01_Introduction/Intro.ts
@@ -0,0 +1,11 @@
+// TypeScript basically makes javascript follow certain rules
+// Typescript was introduced by Micro Soft
+// ts file will converted to js while executing
+let a:string = "patil nagarjuna";
+
+// 1. Keyword -- Export
+// Type 
+let testName:string = "Login test";
+function add(a: number, b: number){
+    return a+b;
+}
